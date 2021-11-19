7b614e244511b0e2c4da551ef0fd668b4d52e9a0
IgorBelak123 <90263299+IgorBelak123@users.noreply.github.com>
2021-11-19 15:24:43 +0100

    Add files via upload

---
+++ command_line_tool/cmake-build-debug/CMakeFiles/command_line_tool.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/command_line_tool.dir/main.cpp.obj"
+  "CMakeFiles/command_line_tool.dir/program.cpp.obj"
+  "command_line_tool.exe"
+  "command_line_tool.exe.manifest"
+  "command_line_tool.pdb"
+  "libcommand_line_tool.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/command_line_tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
